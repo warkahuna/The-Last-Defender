6c69ce97a83a8091fe561a0e6e589475cbe75663
iheb hassine <45923694+warkahuna@users.noreply.github.com>
2020-04-09 21:16:37 +0100

    second

---
+++ Assets/Scripts/MoveTurret.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTurret : MonoBehaviour
+{
+    private Vector3 mousePosition;
+    private Vector3 direction;
+    private Rigidbody turretRigidBody;
+    public float rotateSpeed = 100f;
+    public Camera cam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        turretRigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            //rotate1();
+            rotate2();
+        }
+       
+    }
+
+    void rotate1()
+    {
+        //Get mouse position
+        Vector3 mousePos = Input.mousePosition;
+
+        //Adjust mouse z position
+        mousePos.z = cam.transform.position.y - transform.position.y;
+
+        //Get a world position for the mouse
+        Vector3 mouseWorldPos = cam.ScreenToWorldPoint(mousePos);
+
+        //Get the angle to rotate and rotate
+        float angle = -Mathf.Atan2(transform.position.z - mouseWorldPos.z, transform.position.x - mouseWorldPos.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, angle, 0), rotateSpeed * Time.deltaTime);
+    }
+    void rotate2()
+    {
+        //Get the Screen positions of the object
+        Vector2 positionOnScreen = Camera.main.WorldToViewportPoint(transform.position);
+
+        //Get the Screen position of the mouse
+        Vector2 mouseOnScreen = (Vector2)Camera.main.ScreenToViewportPoint(Input.mousePosition);
+
+        //Get the angle between the points
+        float angle = AngleBetweenTwoPoints(positionOnScreen, mouseOnScreen);
+
+        //Ta Daaa
+        transform.rotation = Quaternion.Euler(new Vector3(0f, -angle, 0f));
+    }
+    float AngleBetweenTwoPoints(Vector3 a, Vector3 b)
+    {
+        return Mathf.Atan2(a.y - b.y, a.x - b.x) * Mathf.Rad2Deg;
+    }
+}
