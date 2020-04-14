b24bd2bf2f3b081a659809243688c122b9a2b755
iheb hassine <45923694+warkahuna@users.noreply.github.com>
2020-04-14 01:55:43 +0100

    third

---
+++ Assets/Scripts/MonsterMovement.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterMovement : MonoBehaviour
+{
+    public Transform Player;
+    public float MoveSpeed = 1;
+    public float MaxDist = 10;
+    public float MinDist = 5;
+    private Animator anim;
+    private bool start = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("z"))
+        {
+            start = true;
+        }
+        if(start)
+        {
+            startWave();
+        }
+    }
+
+   IEnumerator WaitForAnimationToPlay()
+    {
+        yield return new WaitForSeconds(1.30f);
+        
+        anim.ResetTrigger("isWalking");
+        anim.SetTrigger("isRunning");
+    }
+
+    public void startWave()
+    {
+        transform.LookAt(Player);
+
+        if (Vector3.Distance(transform.position, Player.position) >= MinDist)
+        {
+
+            transform.position += transform.forward * MoveSpeed * Time.deltaTime;
+
+            StartCoroutine(WaitForAnimationToPlay());
+
+
+            if (Vector3.Distance(transform.position, Player.position) <= MaxDist)
+            {
+                anim.SetTrigger("isAttacking");
+                anim.ResetTrigger("isRunning");
+
+            }
+        }
+    }
+
+}
--- Assets/Scripts/MoveTurret.cs
+++ Assets/Scripts/MoveTurret.cs
@@ -30,13 +30,13 @@ public class MoveTurret : MonoBehaviour
     {
         //Get mouse position
         Vector3 mousePos = Input.mousePosition;
-
+        
         //Adjust mouse z position
         mousePos.z = cam.transform.position.y - transform.position.y;
 
         //Get a world position for the mouse
         Vector3 mouseWorldPos = cam.ScreenToWorldPoint(mousePos);
-
+        
         //Get the angle to rotate and rotate
         float angle = -Mathf.Atan2(transform.position.z - mouseWorldPos.z, transform.position.x - mouseWorldPos.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, angle, 0), rotateSpeed * Time.deltaTime);
