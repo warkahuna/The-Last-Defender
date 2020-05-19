2afa69be3a2a3877e43922db3b66331d9e6ddf40
Foxs <jawher.gharbi@esprit.tn>
2020-05-19 01:45:11 +0100

    maimMenu+Loading

---
+++ Assets/Scripts/ApplicationManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ApplicationManager : MonoBehaviour {
+	public void Play()
+	{
+
+		SceneManager.LoadScene(1);
+	}
+	public void Quit () 
+	{
+		#if UNITY_EDITOR
+		UnityEditor.EditorApplication.isPlaying = false;
+		#else
+		Application.Quit();
+		#endif
+	}
+}
---
+++ Assets/Scripts/LoadingScrpt.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadingScrpt : MonoBehaviour
+{
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+
+        yield return new WaitForSeconds(5.0f);
+        StartCoroutine(LoadAsyncOperation());
+    }
+
+   IEnumerator LoadAsyncOperation()
+    {
+        AsyncOperation gameLevel = SceneManager.LoadSceneAsync(2);
+       
+        yield return new WaitForEndOfFrame();
+    }
+}
