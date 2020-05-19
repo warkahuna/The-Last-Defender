8e1799f194d9913fae6bf6e6485b51e950c57a09
Foxs <jawher.gharbi@esprit.tn>
2020-05-19 10:57:37 +0100

    Terrain

---
+++ Assets/Scripts/PlaneRotation.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneRotation : MonoBehaviour
+{
+
+    
+    Rigidbody rigidbody;
+   public float speedY, speedZ, torque;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = this.GetComponent<Rigidbody>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rigidbody.AddTorque(transform.up * torque * speedY);
+    }
+    private void FixedUpdate()
+    {
+        rigidbody.AddTorque(transform.up * torque * speedZ);
+    }
+}
